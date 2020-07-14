03422657a0278a7cc1ff8c7b350762e030fe648f
Rand Scullard <rand@randscullard.com>
2020-07-14 15:40:59 -0400

    Make Roboto web font part of the project so that it will be cached by the serviceWorker. Delay rendering the app until web fonts load.

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import WebFont from "webfontloader";
+import "./fonts/Roboto.css";
 import "./index.scss";
 import App from "./app";
 import * as serviceWorker from "./serviceWorker";
 
-WebFont.load({
-	google: {
-		families: ["Roboto:400"]
-	}
-});
+// Delay rendering the app until the web fonts have either loaded or failed to load.
+// This avoids the Flash of Unstyled Text (FOUT) and prevents our React components from
+// using the wrong font metrics when calculating the size of elements.
+let render = () => ReactDOM.render(<App />, document.getElementById("root"));
 
-ReactDOM.render(<App />, document.getElementById("root"));
+WebFont.load({
+	custom: {
+		families: ["Roboto"]
+	},
+	active: render,
+	inactive: render,
+});
 
 serviceWorker.register();
